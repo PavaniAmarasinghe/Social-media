7c4cd10975eab572e273068a010b5c5857e7a98b
Pavani <pavaniyureka06@gmail.com>
2024-05-25 22:08:15 -0700

    modify my social media platform

--- src/Pages/profile/Profile.jsx
+++ src/Pages/profile/Profile.jsx
@@ -8,7 +8,7 @@ import PlaceIcon from "@mui/icons-material/Place";
 import LanguageIcon from "@mui/icons-material/Language";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-
+import Posts from "../../components/posts/Posts"
 const Profile = () => {
   return (
     <div className="profile">
@@ -37,20 +37,21 @@ const Profile = () => {
                     <div className="info">
                         <div className="item">
                           <PlaceIcon/>
-                           <Span>German</Span> 
+                           <span>German</span> 
                         </div>
                         <div className="item">
                           <LanguageIcon/>
-                           <Span>Nova.dev</Span> 
+                           <span>Nova.dev</span> 
                         </div>
-                        <button>follow</button>
                     </div>
+                    <button>follow</button>
                 </div>
                 <div className="right">
                     <EmailOutlinedIcon/>
                     <MoreVertIcon/>
                 </div>
             </div>
+            <Posts/>
         </div>
     </div>
   )
